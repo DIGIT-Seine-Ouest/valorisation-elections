3f7ca5a27fdf84fda7749d0e552f14e92866c5ee
mandresyandri <mandresy.andriantsoanavalona@gmail.com>
2026-01-27 19:58:10 +0100

    feat: pipeline V1 eirel

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,15 @@
+import os
 import json
 import logging
+import requests
 import pandas as pd
+from dotenv import load_dotenv
 
+# Chargement des variables d'environnement
+load_dotenv()
+apikey = os.getenv('api_key')
+
+# Gestion des logs
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     filename="log_recuperation_election.log", 
@@ -10,6 +18,32 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s",
     datefmt='%d/%m/%Y %H:%M:%S'
 )
+
+# Ingestion des données EIREL
+def ingest_eirel(
+    url: str = "https://data.meudon.fr/api/explore/v2.1/catalog/datasets/raw-eirel-municipale/exports/csv",
+    api_key: str = ""
+    ):
+
+    headers = {
+        "Authorization": f"Apikey {api_key}"
+    }
+    params = {
+        "limit": -1,
+        "delimiter": ";",
+        "timezone": "Europe/Paris"
+    }
+
+    # Récupérer les données
+    try:
+        logging.info("Récupération des fichiers EIREL depuis data.meudon.fr")
+        response = requests.get(url, headers=headers, params=params)
+        with open("pipeline/data/bronze/eirel/raw-eirel-municipale.txt", "w") as f:
+            f.write(response.text)
+        logging.info("Dossier chargé dans : data/bronze/eirel/raw-eirel-municipale.txt")
+    except Exception as e:
+        logging.warning(f"Erreur dans la récupération des données. Détail : {e}")
+
 
 # Chargement des données avec les bonnes colonnes
 def load_eirel(filepath, config_path: str='pipeline/data/bronze/reference/eirel_columns.json'):
@@ -29,26 +63,27 @@ def load_eirel(filepath, config_path: str='pipeline/data/bronze/reference/eirel_
 
     data = pd.read_csv(filepath, sep=";", skiprows=2, header=None)
     data.rename(columns=colonnes, inplace=True)
-    logging.info("...")
+    logging.info("EIREL récupéré avec succès.")
 
     return data
 
 # Chargement des donnée 
 def process_eirel(
-    eirel_path: str = "pipeline/data/bronze/eirel/TEST_EIREL_MEUDON_17122025.txt", 
+    eirel_path: str = "pipeline/data/bronze/eirel/raw-eirel-municipale.txt", 
     columns_repartition: str ="pipeline/data/bronze/reference/columns_repartition.json"):
-
+    logging.info("Traitement EIREL...")
     data = load_eirel(eirel_path)
     with open(columns_repartition) as config_cols:
         cols_used = json.load(config_cols)
 
     data[cols_used["metrics_bdv"]].to_csv("pipeline/data/silver/bdv_metrics.csv", index=False)
     data[cols_used["candidats_voix"]].to_csv("pipeline/data/silver/candidats_metrics.csv", index=False)
+    logging.info("EIREL traité avec succès.")
 
 
 
 def enrich_eirel():
-    # Données d'enrichissement --> 
+    logging.info("Enrichissement des données...")
     data = pd.read_csv("pipeline/data/silver/bdv_metrics.csv")
     data_candidates = pd.read_csv("pipeline/data/silver/candidats_metrics.csv")
     bdv = pd.read_csv(
@@ -57,6 +92,7 @@ def enrich_eirel():
         usecols=["num_bureau", "nom_bureau"]
     )
 
+    logging.info("Jointure avec les bureaux de vote...")
     nom_bureau = data.merge(
         bdv, 
         left_on="bureau_vote", 
@@ -72,10 +108,12 @@ def enrich_eirel():
     )
 
     merged_data.to_csv("pipeline/data/gold/election_2025.csv", index=False)
+    logging.info("Enrichissement fait avec succès.")
 
-
+# Lancement de la pipeline
 if __name__ == "__main__":
     logger.info("=== START PIPELINE ===")
+    ingest_eirel(api_key=apikey)
     process_eirel()
     enrich_eirel()
     logger.info("=== END PIPELINE ===")
